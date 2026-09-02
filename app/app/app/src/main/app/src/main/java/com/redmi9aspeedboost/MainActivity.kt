68bb358679fa78156ce9efd718662e63ca89ecbd
chitnyihtwe30-cmyk <chitnyihtwe30@gmail.com>
2026-09-02 19:00:47 +0630

    Update MainActivity.kt

--- app/app/app/src/main/app/src/main/java/com/redmi9aspeedboost/MainActivity.kt
+++ app/app/app/src/main/app/src/main/java/com/redmi9aspeedboost/MainActivity.kt
@@ -9,11 +9,19 @@ class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val text = TextView(this)
-        text.text = "REDMI 9A SPEED BOOST\n\nAPP STARTED"
-        text.textSize = 24f
-        text.setPadding(40, 40, 40, 40)
+        val view = TextView(this)
 
-        setContentView(text)
+        view.text = """
+            REDMI 9A SPEED BOOST
+
+            APP STARTED
+
+            Test Version
+        """.trimIndent()
+
+        view.textSize = 24f
+        view.setPadding(40, 40, 40, 40)
+
+        setContentView(view)
     }
 }
